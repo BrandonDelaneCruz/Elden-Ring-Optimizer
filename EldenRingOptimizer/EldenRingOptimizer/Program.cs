bf6af9283ba286fe7ac71897ea22ea8c0562b696
BrandonCruz <brandondelanecruz@gmail.com>
2024-07-19 02:10:36 -0500

    feat: console write example

--- EldenRingOptimizer/EldenRingOptimizer/Program.cs
+++ EldenRingOptimizer/EldenRingOptimizer/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Hello");
+Console.Write("World");
+Console.Write("FooBar");
+Console.WriteLine();
+
+
+Console.WriteLine("Tell me your name: ");
+var userResponse = Console.ReadLine();
+
+Console.Clear();
+
+Console.Write("Tell me your name: ");
+userResponse = Console.ReadLine();
