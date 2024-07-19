9c521c58e8ca11d4677aa5ad507bd82a6b77b169
BrandonCruz <brandondelanecruz@gmail.com>
2024-07-19 05:53:30 -0500

    feat: class selection

--- EldenRingOptimizer/EldenRingOptimizer/Program.cs
+++ EldenRingOptimizer/EldenRingOptimizer/Program.cs
@@ -29,6 +29,7 @@ while (keepLooping)
     String userResponse = "";
     bool isResponseValid = true;
 
+    #region Character Sheet edit 
     Console.WriteLine($"Charcter name: {characterName}");
     Console.WriteLine($"Class Chosen: {classChosen}");
     Console.WriteLine($"Starting Item: {startingItem}");
@@ -44,6 +45,7 @@ while (keepLooping)
     Console.WriteLine($"Arcane: {arc}");
     Console.WriteLine();
     Console.Write("Would you like to edit this character? (y/n): ");
+    #endregion
 
     while (isResponseValid)
     {
@@ -59,7 +61,6 @@ while (keepLooping)
             Console.WriteLine("3: Starting Item");
             Console.WriteLine("4: Stats");
             Console.Write("Select an option that you want to edit: ");
-
             userResponse = Console.ReadLine();
 
             if (userResponse == "1")
@@ -68,13 +69,13 @@ while (keepLooping)
                 Console.WriteLine($"Character name: {characterName}");
                 Console.Write("New character name: ");
                 characterName = Console.ReadLine();
-                Console.Clear();
 
                 if (userResponse == "1")
                 {
                     isResponseValid = false;
                 }
             }
+            //Class selection
             else if (userResponse == "2")
             {
                 Console.Clear();
@@ -89,60 +90,170 @@ while (keepLooping)
                 Console.WriteLine("8: Vagabond");
                 Console.WriteLine("9: Warrior");
                 Console.WriteLine("10: Wretch");
-                Console.WriteLine("Please select a class: ");
-                
-                if(userResponse = "")
-                {
+                Console.Write("Please select a class: ");
+                userResponse = Console.ReadLine();
+                Console.Clear();
 
+                //Astrologer
+                if (userResponse == "1")
+                {
+                    classChosen = "Astrologer";
+                    vig = "9";
+                    mind = "15";
+                    end = "9";
+                    str = "8";
+                    dex = "12";
+                    intel = "16";
+                    faith = "7";
+                    arc = "9";
                 }
-                if (userResponse = "")
+                //Bandit
+                else if (userResponse == "2")
                 {
-
+                    classChosen = "Bandit";
+                    vig = "10";
+                    mind = "11";
+                    end = "10";
+                    str = "9";
+                    dex = "13";
+                    intel = "9";
+                    faith = "8";
+                    arc = "14";
                 }
-                if (userResponse = "")
+                //Confessor
+                else if (userResponse == "3")
                 {
-
+                    classChosen = "Confessor";
+                    vig = "10";
+                    mind = "13";
+                    end = "10";
+                    str = "12";
+                    dex = "12";
+                    intel = "9";
+                    faith = "14";
+                    arc = "9";
                 }
-                if (userResponse = "")
+                //Hero
+                else if (userResponse == "4")
                 {
-
+                    classChosen = "Hero";
+                    vig = "14";
+                    mind = "9";
+                    end = "12";
+                    str = "16";
+                    dex = "9";
+                    intel = "7";
+                    faith = "8";
+                    arc = "11";
                 }
-                if (userResponse = "")
+                //Prisoner
+                else if (userResponse == "5")
                 {
-
+                    classChosen = "Prisoner";
+                    vig = "11";
+                    mind = "12";
+                    end = "11";
+                    str = "11";
+                    dex = "14";
+                    intel = "14";
+                    faith = "6";
+                    arc = "9";
                 }
-                if (userResponse = "")
+                //Prophet
+                else if (userResponse == "6")
                 {
-
+                    classChosen = "Prophet";
+                    vig = "10";
+                    mind = "14";
+                    end = "8";
+                    str = "11";
+                    dex = "10";
+                    intel = "7";
+                    faith = "16";
+                    arc = "10";
                 }
-                if (userResponse = "")
+                //Samurai
+                else if (userResponse == "7")
                 {
-
+                    classChosen = "Samurai";
+                    vig = "12";
+                    mind = "11";
+                    end = "13";
+                    str = "12";
+                    dex = "15";
+                    intel = "9";
+                    faith = "8";
+                    arc = "8";
                 }
-                if (userResponse = "")
+                //Vagabond
+                else if (userResponse == "8")
                 {
-
+                    classChosen = "Vagabond";
+                    vig = "15";
+                    mind = "10";
+                    end = "11";
+                    str = "14";
+                    dex = "13";
+                    intel = "9";
+                    faith = "9";
+                    arc = "7";
                 }
-                if (userResponse = "")
+                //Warrior
+                else if (userResponse == "9")
                 {
-
+                    classChosen = "Warrior";
+                    vig = "11";
+                    mind = "12";
+                    end = "11";
+                    str = "10";
+                    dex = "16";
+                    intel = "10";
+                    faith = "8";
+                    arc = "9";
                 }
-                if (userResponse = "")
+                //Wretch
+                else if (userResponse == "10")
                 {
-
+                    classChosen = "Wretch";
+                    vig = "10";
+                    mind = "10";
+                    end = "10";
+                    str = "10";
+                    dex = "10";
+                    intel = "10";
+                    faith = "10";
+                    arc = "10";
                 }
                 else
                 {
-
+                    isResponseValid = false;
                 }
             }
             else if (userResponse == "3")
             {
+                Console.Clear();
+                Console.WriteLine("This Feature is not yet Implemented.");
+                Console.Write("Press enter to continue.");
+                userResponse = Console.ReadLine();
+                Console.Clear();
 
+                if(userResponse == "")
+                {
+                    isResponseValid = false;
+                }
             }
             else if (userResponse == "4")
             {
+                Console.Clear();
+                Console.WriteLine("This Feature is not yet Implemented.");
+                Console.Write("Press enter to continue.");
+                userResponse = Console.ReadLine();
+                Console.Clear();
 
+                if (userResponse == "")
+                {
+                    isResponseValid = false;
+                }
             }
             else
             {
